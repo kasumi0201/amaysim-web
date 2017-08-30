b0be595b4363bdad577003aeefb5dd5c9b0d5686
KasumiTakeshima <kasumi0201@gmail.com>
2017-08-30 11:28:02 +1000

    make it better

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def homepage
+  end
+
+  def help
+    @name = 'Kasumi'
+  end
+
+  def randomblah
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'pages#homepage'
+  # root to: 'pages#randomblah'
+
+get 'home', to: 'pages#homepage'
+
+  get 'help/mobile', to: 'pages#help'
+
+  get 'plans/mobile-plans', to: 'plans#mobile'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
